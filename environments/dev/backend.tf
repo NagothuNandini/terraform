599c519530e753082d8c31d24fd6b820991bdf46
root <root@ip-172-31-8-225.ap-south-1.compute.internal>
2026-07-25 04:58:57 +0000

    changing s3 bucket name...

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "dev-terraform-state"
+    bucket       = "nandini-dev-terraform-state"
     key          = "env/dev/terraform.tfstate"
     region       = "ap-south-1"
     use_lockfile = true
